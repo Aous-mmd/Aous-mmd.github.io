8d07e804958a33985630e44b10deabae3daf921b
Aous <aous.mohammad97@gmal.com>
2020-05-31 13:50:31 -0400

    animation home

--- js/main.js
+++ js/main.js
@@ -38,79 +38,109 @@ var links = ["images/Home/items/Hairdressing ServicesBK.jpg",
     "images/Home/items/Henna services.jpeg"
 ]
 $(document).ready(function () {
-    $(window).scroll(function () {
-        var scrollTop = $(window).scrollTop();
-        if (scrollTop >= 309 && scrollTop<311) {
-            $(".s1").animate({
-                right: '0',
-                opacity: '1',
-            }, "slow");
-            $(".s1r").animate({
-                left: '0',
-                opacity: '1',
-            }, "slow");
-        }
-        else if (scrollTop >= 700 && scrollTop < 1100) {
-            $("#img1,#img2,#img3,#img4").animate({
-                left: '0',
-                opacity: '1',
-            }, "slow", function () {
-                $("#1").fadeOut(600);
-                $("#2").fadeOut(600);
-                $("#3").fadeOut(600);
-                $("#4").fadeOut(600);
-            });
-        }
-        else if (scrollTop >= 1100 && scrollTop < 1600) {
-            $("#img5,#img6,#img7,#img8").animate({
-                left: '0',
-                opacity: '1',
-            }, "slow", function () {
-                $("#5").fadeOut(600);
-                $("#6").fadeOut(600);
-                $("#7").fadeOut(600);
-                $("#8").fadeOut(600);
-            });
-        }
-        else if (scrollTop >= 1600 && scrollTop < 2100) {
-            $("#img9,#img10,#img11,#img12").animate({
-                left: '0',
-                opacity: '1',
-            }, "slow", function () {
-                $("#9").fadeOut(600);
-                $("#10").fadeOut(600);
-                $("#11").fadeOut(600);
-                $("#12").fadeOut(600);
-            });
-        }
-        else if (scrollTop >= 2100 && scrollTop < 2750) {
-            $("#img13,#img14,#img15,#img16").animate({
-                left: '0',
-                opacity: '1',
-            }, "slow", function () {
-                $("#13").fadeOut(600);
-                $("#14").fadeOut(600);
-                $("#15").fadeOut(600);
-                $("#16").fadeOut(600);
-            });
-        }
-        else if (scrollTop > 2750) {
-            $("#img17,#img18,#img19").animate({
-                left: '0',
-                opacity: '1',
-            }, "slow", function () {
-                $("#17").fadeOut(600);
-                $("#18").fadeOut(600);
-                $("#19").fadeOut(600);
-            });
-        }
-    });
-    // $('.btn-0').hover(function() {
-    //     // $(this).animate({
-    //     //     background: 'url(../images/hover/gradient-hover.jpg)',
-    //     // });
-    //     $( this ).fadeOut( 100 );
-    //     $( this ).fadeIn( 500 );
-    // });
+    if ($(window).width() >= 960) {
+        $(window).scroll(function () {
+            var scrollTop = $(window).scrollTop();
+            console.log(scrollTop);
+            if (scrollTop >= 700 && scrollTop < 1100) {
+                $("#img1,#img2,#img3,#img4").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#1").fadeOut(600);
+                    $("#2").fadeOut(600);
+                    $("#3").fadeOut(600);
+                    $("#4").fadeOut(600);
+                });
+            }
+            else if (scrollTop >= 1100 && scrollTop < 1600) {
+                $("#img5,#img6,#img7,#img8").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#5").fadeOut(600);
+                    $("#6").fadeOut(600);
+                    $("#7").fadeOut(600);
+                    $("#8").fadeOut(600);
+                });
+            }
+            else if (scrollTop >= 1600 && scrollTop < 2100) {
+                $("#img9,#img10,#img11,#img12").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#9").fadeOut(600);
+                    $("#10").fadeOut(600);
+                    $("#11").fadeOut(600);
+                    $("#12").fadeOut(600);
+                });
+            }
+            else if (scrollTop >= 2100 && scrollTop < 2750) {
+                $("#img13,#img14,#img15,#img16").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#13").fadeOut(600);
+                    $("#14").fadeOut(600);
+                    $("#15").fadeOut(600);
+                    $("#16").fadeOut(600);
+                });
+            }
+            else if (scrollTop > 2750) {
+                $("#img17,#img18,#img19").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#17").fadeOut(600);
+                    $("#18").fadeOut(600);
+                    $("#19").fadeOut(600);
+                });
+            }
+        });
+    }
+    else {
+        $(window).scroll(function () {
+            var scrollTop = $(window).scrollTop();
+            console.log(scrollTop);
+            if (scrollTop >= 1100 && scrollTop < 1767) {
+                $("#img1").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#1").fadeOut(600);
+                });
+            }
+            else if (scrollTop >= 1650 && scrollTop < 2280) {
+                $("#img2").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#2").fadeOut(600);
+                });
+            }
+            else if (scrollTop >= 1600 && scrollTop < 2100) {
+                $("#img9,#img10,#img11,#img12").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#9").fadeOut(600);
+                    $("#10").fadeOut(600);
+                    $("#11").fadeOut(600);
+                    $("#12").fadeOut(600);
+                });
+            }
+            else if (scrollTop >= 2100 && scrollTop < 2750) {
+                $("#img13,#img14,#img15,#img16").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#13").fadeOut(600);
+                    $("#14").fadeOut(600);
+                    $("#15").fadeOut(600);
+                    $("#16").fadeOut(600);
+                });
+            }
+            else if (scrollTop > 2750) {
+                $("#img17,#img18,#img19").animate({
+                    width: '100%'
+                }, "slow", function () {
+                    $("#17").fadeOut(600);
+                    $("#18").fadeOut(600);
+                    $("#19").fadeOut(600);
+                });
+            }
+        });
+    }
 });
 
